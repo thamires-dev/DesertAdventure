ec32038d5c20a972543da0a79828308ee13daba6
thamires-dev <thamicass@gmail.com>
2020-10-05 13:36:52 -0300

    commit

---
+++ scripts/Player.gd
@@ -0,0 +1,42 @@
+extends KinematicBody2D
+
+var MOVE_SPEED = 200
+const JUMP_FORCE = 1100
+const GRAVITY = 50
+const MAX_FALL_SPEED = 500
+
+var y_velo = 0
+
+func _physics_process(delta):
+	var move_dir = 0
+	
+	if Input.is_action_pressed("ui_right"):
+		$Lolla.animation = "run"
+		$Lolla.flip_h = false
+		move_dir += 1
+		
+	elif Input.is_action_pressed("ui_left"):
+		$Lolla.animation = "run"
+		$Lolla.flip_h = true
+		move_dir -= 1
+		
+	else:
+		$Lolla.animation = "idle"
+		
+	var no_chao = $RayCastRight.is_colliding() or $RayCastLeft.is_colliding()
+		
+	move_and_slide(Vector2(move_dir * MOVE_SPEED, y_velo), Vector2(0, -1))
+
+	var grounded = is_on_floor()
+	y_velo += GRAVITY
+	
+	if grounded and Input.is_action_pressed("ui_up"):
+		y_velo = -JUMP_FORCE
+		
+	if grounded and y_velo >= 0:
+		y_velo = 5
+	
+	if y_velo > MAX_FALL_SPEED:
+		y_velo = MAX_FALL_SPEED
+		
+	
